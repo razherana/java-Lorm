d46d8bd5df7dacaa8ce6e613895dfd867a0a851c
razherana <razherana@gmail.com>
2025-03-04 00:07:54 +0300

    Made change so instancerelations still works

--- src/mg/razherana/tests/ModelTests.java
+++ src/mg/razherana/tests/ModelTests.java
@@ -1,72 +1,58 @@
 package mg.razherana.tests;
 
-import java.io.FileWriter;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Arrays;
 
-import mg.razherana.database.DatabaseConnection;
 import mg.razherana.tests.models.User;
 
 public class ModelTests {
-  public static void main(String[] args) {
-    System.out.println("ModelTests");
 
-    try (Connection conn = DatabaseConnection.fromDotEnv("database.xml").getConnection();) {
+    static class Conn {
+        private static Connection conn;
 
-      boolean isInsert = false;
-
-      if (isInsert) {
-        try (FileWriter fileWriter = new FileWriter("data.sql")) {
-          int count = 500;
-          fileWriter.append("INSERT INTO user VALUES");
-          for (int i = 1; i < count; i++) {
-            fileWriter.append(" (" + i + ", 'User " + i + "'),");
-          }
-          fileWriter.append(" (" + count + ", 'User " + count + "');\n");
-          fileWriter.append("INSERT INTO post VALUES");
-          for (int i = 1, p = 1; i < count; i++) {
-            for (int j = 1; j <= count; j++, p++) {
-              fileWriter.append(" (" + p + ", " + i + ", 'Title " + j + "', 'test'),");
-            }
-          }
-          fileWriter.append(" (" + count * count + ", " + count + ", 'Title " + count + "', 'test');");
-        } catch (Exception e) {
-          e.printStackTrace();
+        public static Connection getConn() throws SQLException {
+            return conn == null
+                    ? (conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "razherana", ""))
+                    : conn;
         }
-        return;
-      }
-
-      int testCount = 100;
-      double[] nums = new double[testCount];
-
-      for (int i = 0; i < testCount; i++) {
-
-        long start = System.nanoTime();
-        System.out.println("Request started !!!");
-
-        var ex = new User();
-        ex.addNestedEagerLoad("posts");
-        var exes = ex.all(conn);
-
-        for (var exel : exes) {
-          exel.getPosts(null);
-        }
-
-        nums[i] = ((float) (System.nanoTime() - start)) / 1e6;
-      }
-
-      Arrays.sort(nums);
-
-      System.out.printf("Min = %.2f ms\n", nums[0]);
-      System.out.printf("Max = %.2f ms\n", nums[testCount - 1]);
-
-      double avg = Arrays.stream(nums).average().orElse(0);
-      System.out.printf("Avg = %.2f ms\n", avg);
-
-      conn.close();
-    } catch (SQLException e) {
-      e.printStackTrace();
     }
-  }
+
+    public static void main(String[] args) {
+        System.out.println("ModelTests");
+
+        try (Connection conn = Conn.getConn()) {
+            int testCount = 100;
+            double[] nums = new double[testCount];
+
+            for (int i = 0; i < testCount; i++) {
+
+                long start = System.nanoTime();
+                System.out.println("Request started !!!");
+
+                var ex = new User();
+                ex.addNestedEagerLoad("posts");
+                var exes = ex.all(conn);
+
+                for (var exel : exes) {
+                    exel.getPosts(null);
+                }
+
+                nums[i] = ((float) (System.nanoTime() - start)) / 1e6;
+            }
+
+            Arrays.sort(nums);
+
+            System.out.printf("Min = %.2f ms\n", nums[0]);
+            System.out.printf("Max = %.2f ms\n", nums[testCount - 1]);
+
+            double avg = Arrays.stream(nums).average().orElse(0);
+            System.out.printf("Avg = %.2f ms\n", avg);
+
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/mg/razherana/tests/models/Post.java
+++ src/mg/razherana/tests/models/Post.java
@@ -1,52 +1,54 @@
-// Generated Model using mg.razherana.generator
-// Happy Codingg!
-
 package mg.razherana.tests.models;
 
-import java.sql.SQLException;
-import mg.razherana.lorm.annot.columns.Column;
-import java.sql.Connection;
-import mg.razherana.lorm.annot.relations.BelongsTo;
-import mg.razherana.lorm.annot.relations.HasMany;
-import mg.razherana.lorm.annot.general.Table;
-import mg.razherana.lorm.annot.columns.ForeignColumn;
 import mg.razherana.lorm.Lorm;
-import java.util.ArrayList;
+import mg.razherana.lorm.annot.columns.Column;
+import mg.razherana.lorm.annot.columns.ForeignColumn;
+import mg.razherana.lorm.annot.general.Table;
 
 @Table("post")
-@BelongsTo(model = User.class, foreignKey = "user", relationName = "user")
-@HasMany(model = Comment.class, foreignKey = "post", relationName = "comments")
-public class Post extends Lorm<Post> { 
-	@Column(value = "description", getter = "getDescription", setter = "setDescription")
-	private String description;
+public class Post extends Lorm<Post> {
+    @Column(value = "id", primaryKey = true)
+    private int id;
 
-	@Column(value = "id", primaryKey = true, getter = "getId", setter = "setId")
-	private int id;
+    @Column("user")
+    @ForeignColumn(name = "id", model = User.class)
+    private int user;
 
-	@Column(value = "title", getter = "getTitle", setter = "setTitle")
-	private String title;
+    @Column("title")
+    private String title;
 
-	@Column(value = "user", getter = "getUser", setter = "setUser")
-	@ForeignColumn(name = "id", model = User.class)
-	private int user;
+    @Column("description")
+    private String description;
 
-	public String getDescription() { return description; }
+    public int getId() {
+        return id;
+    }
 
-	public void setDescription(String description) { this.description = description; }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public int getId() { return id; }
+    public int getUser() {
+        return user;
+    }
 
-	public void setId(int id) { this.id = id; }
+    public void setUser(int user) {
+        this.user = user;
+    }
 
-	public String getTitle() { return title; }
+    public String getTitle() {
+        return title;
+    }
 
-	public void setTitle(String title) { this.title = title; }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public int getUser() { return user; }
+    public String getDescription() {
+        return description;
+    }
 
-	public void setUser(int user) { this.user = user; }
-
-	public User getUser(Connection connection) throws SQLException { return belongsTo("user", connection); }
-
-	public ArrayList<Comment> getComments(Connection connection) throws SQLException { return hasMany("comments", connection); }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
